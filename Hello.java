27291799e6858980cecd84164583f56685e5e257
ali taha <ataha1@stuy.edu>
2018-10-17 11:01:05 -0400

    me

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+  public static void main(String[] args){
+    String x="Ali Taha";
+    String y="Greetings";
+    System.out.println("Hello my name is "+x+", " +y);
+
+
+
+  }
+}
